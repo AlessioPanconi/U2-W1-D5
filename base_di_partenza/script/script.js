78ae4d046eb2491f98552fe1f8899c0e959c6b0a
Alessio Panconi <apanconi94@gmail.com>
2025-04-11 15:59:21 +0200

    Modifiche per nav bar cambio colore

---
+++ base_di_partenza/script/script.js
@@ -0,0 +1,15 @@
+const yelloSection = document.getElementById("yellowSection");
+const navbar = document.getElementById("header");
+const headerbutton = document.getElementById("headerButton");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > yellowSection.offsetHeight) {
+    yellowSection.classList.add("scrolled");
+    navbar.classList.add("scrolled");
+    headerButton.classList.add("scrolled1");
+  } else {
+    yellowSection.classList.remove("scrolled");
+    navbar.classList.remove("scrolled");
+    headerButton.classList.remove("scrolled1");
+  }
+});
